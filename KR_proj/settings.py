d38113be90369dd431583d4607d60f4630c781b8
yTripper <149474382+yTripper@users.noreply.github.com>
2025-06-23 13:31:11 +0300

    abexam - Step 4

--- KR_proj/settings.py
+++ KR_proj/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webexamapp',
+    'examapp',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
--- examapp/admin.py
+++ examapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import abexam
 
-# Register your models here.
+class AbexamAdmin(admin.ModelAdmin):
+    list_display = ('exam_name', 'exam_date', 'is_public', 'created_at')
+    search_fields = ('exam_name', 'users__email')
+    list_filter = ('is_public', 'created_at', 'exam_date')
+    filter_horizontal = ('users',)
+    date_hierarchy = 'exam_date'
+
+admin.site.register(abexam, AbexamAdmin)
