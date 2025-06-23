425d82ff15d75d18bca79c95163c2369d8d25da8
yTripper <149474382+yTripper@users.noreply.github.com>
2025-06-23 14:04:39 +0300

    abexam - Step 6

--- KR_proj/urls.py
+++ KR_proj/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from examapp.views import abexam_step6
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('abexam/', abexam_step6, name='abexam'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- examapp/views.py
+++ examapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import abexam
 
 # Create your views here.
+
+def abexam_step6(request):
+    exams = abexam.objects.filter(is_public=True)
+    return render(request, 'abexam_step6.html', {
+        'exams': exams,
+    })
